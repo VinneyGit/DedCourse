1c8ac944e5763d367aa7a63f9e6e83a624fbed8c
VinneyGit <pchelkinilja@yandex.ru>
2026-09-04 00:43:29 +0300

    added isdigit, isspace, atoi, atof(in process), strdup

--- lesson1/MyString.c
+++ lesson1/MyString.c
@@ -1,15 +1,33 @@
-#include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 size_t Strlen(const char* str);
+
 int Puts(const char* str);
+
 char* Strcpy(char* destination, const char* source);
+
 char* Strcat(char* destination, const char* source);
+
 int Strcmp(const char* leftstr, const char* rightstr);
 
+int Isdigit(int ch);
+
+int Isspace(int ch);
+
+int Atoi(const char* str);
+
+double Atof(const char* str);
+
+char* Strdup(const char* str); // After using always use free()
+
+
 int main() {
-    printf("TEST: %d, %d\n", Strcmp("atg35", "aa35"), strcmp("atg35", "aa35"));
+    // char str[80] = "\0";
+    // scanf("%s", str);
+    printf("TEST: %g\n", atof("iNffe"));
     return 0;
 }
 
@@ -66,10 +84,72 @@ int Strcmp(const char* leftstr, const char* rightstr) {
 
     return leftstr[i] - rightstr[i];
 }
+
+
+int Isdigit(int ch) {
+    return (ch >= '0' && ch <= '9') ? 1 : 0;
+}
+
+
+int Isspace(int ch) {
+    if ((ch >= '\t' && ch <= '\r') || ch == ' ') {
+        return 1;
+    }
+    return 0;
+}
+
+
+int Atoi(const char* str) {
+    int number = 0;
+    int i = 0;
+    int sign = 1;
+
+    while (Isspace(str[i])) {
+        i++;
+    }
+
+    if (str[i] == '-') {
+        sign = -1;
+        i++;
+    }
+    else if (str[i] == '+') {
+        i++;
+    }
+
+    while (Isdigit(str[i])) {
+        number *= 10;
+        number += str[i] - '0';
+        i++;
+    }
+
+    return sign * number;
+}
+
+
+double Atof(const char* str) { // in process 1. add inf and nan handling 2. add
+    double number = 0.0;
+    int i = 0;
+    int sign = 1;
+
+    while (Isspace(str[i])) {
+        i++;
+    }
+
+    if (str[i] == '-') {
+        sign = -1;
+        i++;
+    }
+
+    return sign * number;
+}
+
+
+char* Strdup(const char* str) {
+    char* duplicate = (char*)malloc(sizeof(str));
+    Strcpy(duplicate, str);
+    return duplicate;
+}
 /*
-strdup 3
 getline 4
-atoi 1
-atof 2
 +n
 */
